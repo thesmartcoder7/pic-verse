185e27e80f21b6610a62e632bd4b1cdb1d5f4a4d
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-05 20:03:18 +0300

    style: stucture and style the signup page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
+from unittest import result
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+    return render(request, 'users/signup.html')
+
+def login(request):
+    return render(request, 'users/login.html')
