dfe4ac1bd0fffb7e0f5d2b983865004dc6d086ac
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-05 18:21:46 +0300

    chore: initia user app setup

--- instagram/settings.py
+++ instagram/settings.py
@@ -36,13 +36,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
