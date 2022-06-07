640fd551fa5325723089b0813533ec06baf1d2af
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-08 00:49:38 +0300

    style: structure and style the post creation modal

--- app/views.py
+++ app/views.py
@@ -30,24 +30,73 @@ def home(request):
 
 @login_required
 def profile(request):
-    return render(request, 'app/profile.html')
+    post_form = PostCreationForm()
+    if request.method == 'POST':
+        post_form = PostCreationForm(request.POST, request.FILES, instance=request.user )
+        context = {
+            'post_form': post_form
+        }
+        if post_form.is_valid():
+            print('\n\nform gets to be validated\n\n')
+            post_form.save()
+            print(Post.objects.all())
+            return redirect('insta-home')
+        else:
+            print('\n\nthe form is not validated\n\n')
+            return render(request, 'app/profile.html', context)
+    else:
+        post_form = PostCreationForm()
+        print('\n\nthe request is not even a post request\n\n')
+        context = {
+            'post_form': post_form
+        }
+        return render(request, 'app/profile.html', context)
+
 
 @login_required
 def explore(request):
-    return render(request, 'app/explore.html')
+    post_form = PostCreationForm()
+    if request.method == 'POST':
+        post_form = PostCreationForm(request.POST, request.FILES, instance=request.user )
+        context = {
+            'post_form': post_form
+        }
+        if post_form.is_valid():
+            print('\n\nform gets to be validated\n\n')
+            post_form.save()
+            print(Post.objects.all())
+            return redirect('insta-home')
+        else:
+            print('\n\nthe form is not validated\n\n')
+            return render(request, 'app/explore.html', context)
+    else:
+        post_form = PostCreationForm()
+        print('\n\nthe request is not even a post request\n\n')
+        context = {
+            'post_form': post_form
+        }
+        return render(request, 'app/explore.html', context)
+
 
 @login_required
 def settings(request):
     if request.method == 'POST':
+        post_form = PostCreationForm()
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
         context = {
             'u_form': u_form,
-            'p_form': p_form
+            'p_form': p_form,
+            'post_form': post_form
         }
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
+            return redirect('insta-home')
+        elif post_form.is_valid():
+            print('\n\nform gets to be validated\n\n')
+            post_form.save()
+            print(Post.objects.all())
             return redirect('insta-home')
         else:
             
@@ -55,13 +104,35 @@ def settings(request):
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
+        post_form = PostCreationForm(request.POST, request.FILES, instance=request.user )
         context = {
-            'u_form': u_form,
-            'p_form': p_form
+             'u_form': u_form,
+            'p_form': p_form,
+            'post_form': post_form
         }
         return render(request, 'app/settings.html', context)
 
 
 @login_required
 def igtv(request):
-    return render(request, 'app/igtv.html')
+    post_form = PostCreationForm()
+    if request.method == 'POST':
+        post_form = PostCreationForm(request.POST, request.FILES, instance=request.user )
+        context = {
+            'post_form': post_form
+        }
+        if post_form.is_valid():
+            print('\n\nform gets to be validated\n\n')
+            post_form.save()
+            print(Post.objects.all())
+            return redirect('insta-home')
+        else:
+            print('\n\nthe form is not validated\n\n')
+            return render(request, 'app/igtv.html', context)
+    else:
+        post_form = PostCreationForm()
+        print('\n\nthe request is not even a post request\n\n')
+        context = {
+            'post_form': post_form
+        }
+        return render(request, 'app/igtv.html', context)
