c869efa524e4ac43328f61acb965007f254bd218
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-08 11:06:37 +0300

    chore: pass data into the templates

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('', include('app.urls')),
     path('users/', include('users.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
