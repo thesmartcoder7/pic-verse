c256ae0035dccf89163e9ba03f7ae8f8c138ed86
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2022-10-17 19:10:52 +0300

    refactor: recover repo files and add TypeScript to codebase

--- app/static/app/js/main.js
+++ app/static/app/js/main.js
@@ -1,61 +1,65 @@
 let postSections = document.querySelectorAll(".profile");
 let profileNavs = document.querySelectorAll(".p-nav");
-
 let hoverimages = document.querySelectorAll(".display_image");
 let imageCounters = document.querySelectorAll(".counters");
-
 let modalClosers = document.querySelectorAll(".close");
-
 let postModal = document.querySelector(".post-modal");
 let createPostToggle = document.querySelector(".toggle-add-post");
-
-document.querySelector(".disable-post-button").addEventListener("click", () => {
-  createPostToggle.style.display = none;
-});
-
-createPostToggle.addEventListener("click", (e) => {
-  postModal.style.display = "flex";
-});
-
-for (let nav of profileNavs) {
-  nav.addEventListener("click", (e) => {
+let disablePost = document.querySelector(".disable-post-button");
+if (disablePost) {
+    disablePost.addEventListener("click", () => {
+        if (createPostToggle) {
+            createPostToggle.style.display = "none";
+        }
+    });
+}
+if (createPostToggle) {
+    createPostToggle.addEventListener("click", (e) => {
+        if (postModal) {
+            postModal.style.display = "flex";
+        }
+    });
+}
+if (profileNavs) {
     for (let nav of profileNavs) {
-      nav.classList.remove("active");
+        nav.addEventListener("click", (e) => {
+            for (let nav of profileNavs) {
+                nav.classList.remove("active");
+            }
+            e.target.classList.add("active");
+            for (let section of postSections) {
+                section.classList.remove("active");
+                if (section.classList.contains(e.target.textContent.toLowerCase())) {
+                    section.classList.add("active");
+                }
+            }
+        });
     }
-    e.target.classList.add("active");
-
-    for (section of postSections) {
-      section.classList.remove("active");
-      if (section.classList.contains(e.target.textContent.toLowerCase())) {
-        section.classList.add("active");
-      }
+}
+if (hoverimages) {
+    for (let image of hoverimages) {
+        image.addEventListener("mouseover", (e) => {
+            e.target.nextElementSibling.style.display = "flex";
+        });
+        image.addEventListener("click", (e) => {
+            console.log(e);
+        });
     }
-  });
 }
-
-for (let image of hoverimages) {
-  image.addEventListener("mouseover", (e) => {
-    e.target.nextElementSibling.style.display = "flex";
-  });
-
-  image.addEventListener("click", (e) => {
-    console.log(e);
-  });
+if (imageCounters) {
+    for (let counter of imageCounters) {
+        counter.addEventListener("mouseleave", (e) => {
+            e.target.style.display = "none";
+        });
+        counter.addEventListener("click", (e) => {
+            e.target.nextElementSibling.style.display = "flex";
+        });
+    }
 }
-
-for (let counter of imageCounters) {
-  counter.addEventListener("mouseleave", (e) => {
-    e.target.style.display = "none";
-  });
-
-  counter.addEventListener("click", (e) => {
-    e.target.nextElementSibling.style.display = "flex";
-  });
-}
-
-for (let closer of modalClosers) {
-  closer.addEventListener("click", (e) => {
-    console.log(e);
-    e.target.parentElement.style.display = "none";
-  });
+if (modalClosers) {
+    for (let closer of modalClosers) {
+        closer.addEventListener("click", (e) => {
+            e.target.parentElement.style.display = "none";
+        });
+    }
 }
