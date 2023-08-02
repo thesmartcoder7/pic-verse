3f5c5b1d40772765a5f8d0d42b9625c030600081
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-08-02 20:41:57 +0300

    docs: update user views documentation and make static files for deployment

--- users/views.py
+++ users/views.py
@@ -10,13 +10,27 @@ from app.forms import *
 
 
 def signup(request):
+    """
+    View function for user signup.
+
+    If the request method is POST, it attempts to process the user signup form.
+    If the form is valid, it saves the user and redirects to the user login page.
+    If the form is not valid, it renders the signup page with the form and error messages.
+
+    If the request method is GET, it renders the signup page with an empty signup form.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+    """
     if request.method == 'POST':
         form = UserSignupForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('user-login')
         else:
-
             return render(request, 'users/signup.html', {'form': form})
     else:
         form = UserSignupForm()
@@ -24,6 +38,19 @@ def signup(request):
 
 
 def follow_count(user_id, followee_id):
+    """
+    Get the count of followers for a user and followees for a followee.
+
+    Args:
+        user_id (int): The ID of the user.
+        followee_id (int): The ID of the followee.
+
+    Returns:
+        list: A list containing the count of followers for the user,
+              the appropriate text label for the followers count,
+              the count of followees for the followee,
+              and the appropriate text label for the followees count.
+    """
     userFollows = Follow.objects.filter(follower=user_id)
     followeeFollows = Follow.objects.filter(following=followee_id)
     u_following_a = []
@@ -53,6 +80,21 @@ def follow_count(user_id, followee_id):
 
 @login_required
 def follow(request, user1, user2):
+    """
+    View function to handle following/unfollowing another user.
+
+    This view requires the user to be logged in. It allows the currently logged-in user
+    to follow or unfollow another user.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        user1 (str): The username of the follower user.
+        user2 (str): The username of the followee user.
+
+    Returns:
+        HttpResponse: The HTTP response object containing a JSON response.
+        The JSON response includes status information, follower counts, and text labels.
+    """
     user1 = User.objects.filter(username=user1).first()
     user2 = User.objects.filter(username=user2).first()
     followee_profile = Profile.objects.filter(user=user2).first()
@@ -60,7 +102,7 @@ def follow(request, user1, user2):
 
     response = {
         'status': False,
-        'message': 'An issuer occured!',
+        'message': 'An issue occurred!',
     }
 
     if follow:
